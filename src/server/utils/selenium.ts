fa4ad3e54fde8fefaf36174585ad28e324e613f9
Justin Murrah <jdmurrah@gmail.com>
2023-02-19 21:03:23 -0600

    changed it back to google search

--- src/server/utils/selenium.ts
+++ src/server/utils/selenium.ts
@@ -3,14 +3,15 @@ import chrome from "chromedriver";
 
 export const googleSearch = () => {
   return new Promise(async (resolve, reject) => {
-    // const searchString = "Selenium Test";
+    const searchString = "Selenium Test";
 
     try {
       const driver = await new Builder().forBrowser("chrome").build();
-      driver.get("https://lastremnant.fandom.com/wiki/The_Last_Remnant_Wiki_-_The_Last_Remnant_Guide");
-      await driver.findElement(By.css("td.top")).takeScreenshot();
-      //   let title = await driver.getTitle();
-      //   console.log(`The Title is: ${title}`);
+      driver.get("http://google.com");
+      await driver.findElement(By.name("q")).sendKeys(searchString, Key.RETURN);
+      let title = await driver.getTitle();
+      console.log(`The Title is: ${title}`);
+      resolve(`The Title is: ${title}`);
       await driver.quit();
     } catch (error) {
       console.log(error);
